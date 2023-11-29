1567c6f13725c6300382966f2aa1d0a58333ecf1
Nina Laouadi <nina.laouadi0@gmail.com>
2023-11-29 16:55:10 +0100

    simple airbnb to finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating Flats..."
+lovely_flat_in_marseille = {name: "Lovely Flat In Marseille", address: "13004 - Marseille", description: "49m2, with a balcony", price_per_night: 70, number_of_guests: 3}
+great_loft_in_lille = {name: "Great Loft In Lille", address: "59800 - Lille", description: "Loft 100m2, with a rooftop", price_per_night: 150, number_of_guests: 8}
+little_flat_in_paris = {name: "Little Flat In Paris", address: "75012 - Paris", description: "Chambre de bonne, 9m2", price_per_night: 100, number_of_guests: 2}
+nice_flat_in_paris = {name: "Nice Flat In Paris", address: "75011 - Paris", description: "Best Neighbourhood", price_per_night: 150, number_of_guests: 4}
+big_flat_in_marseille = {name: "Big Flat In Marseille", address: "13001 - Marseille", description: "On the Vieux Port", price_per_night: 150, number_of_guests: 6}
+
+
+[lovely_flat_in_marseille, great_loft_in_lille , little_flat_in_paris, nice_flat_in_paris , big_flat_in_marseille].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
